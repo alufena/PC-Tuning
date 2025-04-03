f1bfca783bd7134ea1910849c84c0469e4c5a22e
alufena <55760228+alufena@users.noreply.github.com>
2025-04-03 17:12:33 -0300

    Delete Restart Spotify & Ezblocker.bat

--- Restart Spotify & Ezblocker.bat
+++
@@ -1,5 +0,0 @@
-taskkill /f /im Spotify.exe
-taskkill /f /im EZBlocker.exe
-start "" "C:\Users\Administrator\AppData\Roaming\Spotify\Spotify.exe"
-start "" "C:\Program Files (x86)\EZBlocker\old 2\EZBlocker.exe"
-exit
