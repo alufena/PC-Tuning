97aa89cab8f9bc308ae0f1128d7e5a453e693d35
alufena <55760228+alufena@users.noreply.github.com>
2025-04-13 14:44:16 -0300

    Update Flush DNS.bat

--- Flush DNS.bat
+++ Flush DNS.bat
@@ -4,9 +4,4 @@ netsh int ipv4 reset reset.log
 netsh int ipv6 reset reset.log
 netsh winsock reset
 netsh int ip reset
-::netsh int tcp set global autotuninglevel=disable
-::netsh int tcp set global nonsackrttresiliency=disabled
-::netsh int tcp set global ecncapability=disabled
-::netsh int tcp set global timestamps=disabled
-::netsh int tcp set supplemental custom tcpackfrequency=1
 exit
