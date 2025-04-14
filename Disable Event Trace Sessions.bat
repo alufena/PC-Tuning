9e3940e7020571438fd23de4c0c9a6d39bb02432
alufena <55760228+alufena@users.noreply.github.com>
2025-04-14 01:34:12 -0300

    Add files via upload

---
+++ Disable Event Trace Sessions.bat
@@ -0,0 +1,32 @@
+@echo off
+:: Disable Event Trace Sessions (ETS)
+for %%X in (
+    "NTFSLog"
+    "WiFiDriverIHVSession"
+    "WiFiDriverSession"
+    "WiFiSession"
+    "SleepStudyTraceSession"
+    "1DSListener"
+    "MpWppTracing"
+    "NVIDIA-NVTOPPS-NoCat"
+    "NVIDIA-NVTOPPS-Filter"
+    "Circular Kernel Context Logger"
+    "DiagLog"
+    "LwtNetLog"
+    "Microsoft-Windows-Rdp-Graphics-RdpIdd-Trace"
+    "NetCore"
+    "RadioMgr"
+    "ReFSLog"
+    "WdiContextLog"
+    "ShadowPlay"
+	"Eventlog-Security"
+    "GraphicsPerfMonitorSession"
+    "UBPM"
+    "UserNotPresentTraceSession"
+    "WindowsUpdate_trace_log"
+    "Steam Event Tracing"
+
+) do (
+    logman stop %%X -ets
+)
+pause
