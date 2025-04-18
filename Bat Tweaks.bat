734fac971113b8286dd27161f40aaa8c05b01d0d
alufena <55760228+alufena@users.noreply.github.com>
2025-04-18 06:10:35 -0300

    Update Bat Tweaks.bat

--- Bat Tweaks.bat
+++ Bat Tweaks.bat
@@ -1,5 +1,5 @@
 fsutil behavior set disable8dot3 1
-fsutil behavior set disablelastaccess 3
+fsutil behavior set disablelastaccess 1
 fsutil behavior set disableencryption 1
 fsutil behavior set memoryusage 2
 manage-bde -off C:
