a9f6c39c18107c38b5ac53ab90a6f05cbaec9de4
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-04-25 00:22:03 -0300

    Update Bat Tweaks.bat

--- Bat Tweaks.bat
+++ Bat Tweaks.bat
@@ -2,6 +2,8 @@ fsutil behavior set disable8dot3 1
 fsutil behavior set disablelastaccess 1
 fsutil behavior set disableencryption 1
 fsutil behavior set memoryusage 2
+fsutil behavior set encryptpagingfile 0
+fsutil behavior set mftzone 4
 manage-bde -off C:
 manage-bde -off D:
 manage-bde -off E:
