0ce9ddad33309e6b3ab8671f7b6440abcd86607c
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-04-27 00:42:59 -0300

    Update Disable Event Trace Sessions.bat

--- Disable Event Trace Sessions.bat
+++ Disable Event Trace Sessions.bat
@@ -1,5 +1,7 @@
 @echo off
-:: Disable Event Trace Sessions (ETS)
+chcp 65001 >nul
+:: Disable Event Trace Sessions (ETS) - Ultra rápido, sem consultar antes
+
 for %%X in (
     "NTFSLog"
     "WiFiDriverIHVSession"
@@ -19,14 +21,21 @@ for %%X in (
     "ReFSLog"
     "WdiContextLog"
     "ShadowPlay"
-	"Eventlog-Security"
-    "GraphicsPerfMonitorSession"
+    "Eventlog-Security"
+    "EventLog-Application"
     "UBPM"
+    "GraphicsPerfMonitorSession"
+    "F589D420-8B9A-40A2-95A8-DBB27ED17997"
+    "BE235AAC-AC9D-4DDC-9FA6-D4C37AF837B7"
+    "D425B399-4B71-4151-8DAF-413F92A75EC3"
     "UserNotPresentTraceSession"
     "WindowsUpdate_trace_log"
     "Steam Event Tracing"
-
 ) do (
-    logman stop %%X -ets
+    start "" /b cmd /c ^
+    "logman stop %%X -ets >nul 2>&1 && echo [OK] Sessão %%X parada."
 )
+
+timeout /t 2 /nobreak
+
 exit
