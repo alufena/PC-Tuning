3bc9f7f79523111cf9c882eb5cb57b9fed17952b
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-05-20 20:06:37 -0300

    Update Kill HardAcc.bat

--- Kill HardAcc.bat
+++ Kill HardAcc.bat
@@ -1,98 +1,98 @@
-taskkill /f /t /im video.ui.exe
-taskkill /f /t /im msiexec.exe
-taskkill /f /t /im yourphone.exe
-taskkill /f /t /im startmenuexperiencehost.exe
-taskkill /f /t /im searchhost.exe
-taskkill /f /t /im msedgewebview2.exe
-taskkill /f /t /im memBoost.exe
-taskkill /f /t /im microsoft.photos.exe
-taskkill /f /t /im winstore.app.exe
-taskkill /f /t /im textinputhost.exe
-taskkill /f /t /im GameManagerService.exe
-taskkill /f /t /im AppVShNotify.exe
-taskkill /f /t /im Agent.exe
-taskkill /f /t /im calculator.exe
-taskkill /f /t /im lockapp.exe
-taskkill /f /t /im TabTip.exe
-taskkill /f /t /im hxoutlook.exe
-taskkill /f /t /im CEPHtmlEngine.exe
-taskkill /f /t /im hxaccounts.exe
-taskkill /f /t /im JackettConsole.exe
-taskkill /f /t /im CHXSmartScreen.exe
-taskkill /f /t /im SDXHelper.exe
-taskkill /f /t /im CompPkgSrv.exe
-taskkill /f /t /im AdobeIPCBroker.exe
-taskkill /f /t /im CoreSync.exe
-taskkill /f /t /im set-up.exe
-taskkill /f /t /im CCXProcess.exe
-taskkill /f /t /im CCLibrary.exe
-taskkill /f /t /im CrashMailer_64.exe
-taskkill /f /t /im gamebar.exe
-taskkill /f /t /im FESearchHost.exe
-taskkill /f /t /im smartscreen.exe
-taskkill /f /t /im Widgets.exe
-taskkill /f /t /im SecurityHealthService.exe
-taskkill /f /t /im HelpPane.exe
-taskkill /f /t /im ai.exe
-taskkill /f /t /im GameBarPresenceWriter.exe
-taskkill /f /t /im SelectiveToolApp.exe
-taskkill /f /t /im PhoneExperienceHost.exe
-taskkill /f /t /im MicrosoftEdgeUpdate.exe
-taskkill /f /t /im CalculatorApp.exe
-taskkill /f /t /im PhotosService.exe
-taskkill /f /t /im WidgetService.exe
-taskkill /f /t /im adb.exe
-taskkill /f /t /im VSSVC.exe
-taskkill /f /t /im cfbackd.w32.exe
-taskkill /f /t /im WmiPrvSE.exe
-taskkill /f /t /im helperservice.exe
-taskkill /f /t /im SnippingTool.exe
-taskkill /f /t /im fdm.exe
-taskkill /f /t /im OSE.exe
-taskkill /f /t /im HxTsr.exe
-taskkill /f /t /im GameInputSvc.exe
-taskkill /f /t /im EABackgroundService.exe
-taskkill /f /t /im WindowsPackageManagerServer.exe
-taskkill /f /t /im rundll32.exe
-taskkill /f /t /im armsvc.exe
-taskkill /f /t /im "Razer Central.exe"
-taskkill /f /t /im "Razer Synapse 3.exe"
-taskkill /f /t /im "Razer Synapse Service Process.exe"
-taskkill /f /t /im "Razer Synapse Service.exe"
-taskkill /f /t /im "RazerCentralService.exe"
-taskkill /f /t /im NVDisplay.Container.exe
 taskkill /f /t /im acrotray.exe
+taskkill /f /t /im adb.exe
+taskkill /f /t /im AdobeIPCBroker.exe
+taskkill /f /t /im Agent.exe
+taskkill /f /t /im ai.exe
+taskkill /f /t /im ApplicationFrameHost.exe
+taskkill /f /t /im AppVShNotify.exe
+taskkill /f /t /im armsvc.exe
+taskkill /f /t /im backgroundTaskHost.exe
+taskkill /f /t /im CalculatorApp.exe
+taskkill /f /t /im calculator.exe
+taskkill /f /t /im CCLibrary.exe
+taskkill /f /t /im CCXProcess.exe
+taskkill /f /t /im CEPHtmlEngine.exe
+taskkill /f /t /im cfbackd.w32.exe
+taskkill /f /t /im CHXSmartScreen.exe
+taskkill /f /t /im CompPkgSrv.exe
+taskkill /f /t /im CoreSync.exe
+taskkill /f /t /im CrashMailer_64.exe
+taskkill /f /t /im DashboardNotificationManager.exe
+taskkill /f /t /im DataExchangeHost.exe
+taskkill /f /t /im EABackgroundService.exe
+taskkill /f /t /im fdm.exe
+taskkill /f /t /im FESearchHost.exe
+taskkill /f /t /im FileCoAuth.exe
+taskkill /f /t /im gamebar.exe
+taskkill /f /t /im GameBarPresenceWriter.exe
+taskkill /f /t /im GameInputSvc.exe
+taskkill /f /t /im GameManagerService.exe
+taskkill /f /t /im helperservice.exe
+taskkill /f /t /im HelpPane.exe
+taskkill /f /t /im hxaccounts.exe
+taskkill /f /t /im hxoutlook.exe
+taskkill /f /t /im HxTsr.exe
+taskkill /f /t /im JackettConsole.exe
+taskkill /f /t /im jcef_helper.exe
+taskkill /f /t /im lockapp.exe
+taskkill /f /t /im memBoost.exe
+taskkill /f /t /im MicrosoftEdgeUpdate.exe
+taskkill /f /t /im microsoft.photos.exe
+taskkill /f /t /im MpCmdRun.exe
+taskkill /f /t /im msedgewebview2.exe
+taskkill /f /t /im msiexec.exe
+taskkill /f /t /im NVDisplay.Container.exe
+taskkill /f /t /im OSE.exe
+taskkill /f /t /im PerfWatson2.exe
+taskkill /f /t /im PhoneExperienceHost.exe
+taskkill /f /t /im PhotosService.exe
+taskkill /f /t /im "Razer Central.exe"
+taskkill /f /t /im "RazerCentralService.exe"
+taskkill /f /t /im "Razer Synapse 3.exe"
+taskkill /f /t /im "Razer Synapse Service.exe"
+taskkill /f /t /im "Razer Synapse Service Process.exe"
+taskkill /f /t /im RtkAudUService64.exe
+taskkill /f /t /im RtkNGUI64.exe
+taskkill /f /t /im rundll32.exe
+taskkill /f /t /im SDXHelper.exe
+taskkill /f /t /im searchhost.exe
+taskkill /f /t /im SecurityHealthHost.exe
+taskkill /f /t /im SecurityHealthService.exe
+taskkill /f /t /im SelectiveToolApp.exe
+taskkill /f /t /im set-up.exe
+taskkill /f /t /im smartscreen.exe
+taskkill /f /t /im smss.exe
+taskkill /f /t /im SnippingTool.exe
 taskkill /f /t /im splwow64.exe
 taskkill /f /t /im spoolsv.exe
-taskkill /f /t /im TiWorker.exe
-taskkill /f /t /im TrustedInstaller.exe
-taskkill /f /t /im backgroundTaskHost.exe
-taskkill /f /t /im vds.exe
-taskkill /f /t /im SystemSettingsBroker.exe
-taskkill /f /t /im FileCoAuth.exe
-taskkill /f /t /im uhssvc.exe
-taskkill /f /t /im RtkNGUI64.exe
-taskkill /f /t /im PerfWatson2.exe
-taskkill /f /t /im sqlwriter.exe
-taskkill /f /t /im DashboardNotificationManager.exe
-taskkill /f /t /im jcef_helper.exe
-taskkill /f /t /im DataExchangeHost.exe
-taskkill /f /t /im SecurityHealthHost.exe
-taskkill /f /t /im ApplicationFrameHost.exe
-taskkill /f /t /im vssadmin.exe
-taskkill /f /t /im werfault.exe
 taskkill /f /t /im sppsvc.exe
-taskkill /f /t /im smss.exe
-taskkill /f /t /im WUDFHost.exe
-taskkill /f /t /im MpCmdRun.exe
-taskkill /f /t /im WmiApSrv.exe
+taskkill /f /t /im sqlwriter.exe
 taskkill /f /t /im ssh-agent.exe
 taskkill /f /t /im ssh.exe
-taskkill /f /t /im RtkAudUService64.exe
-::sc stop "SysMain"
-::sc config "SysMain" start= disabled
+taskkill /f /t /im startmenuexperiencehost.exe
+taskkill /f /t /im SystemSettingsBroker.exe
+taskkill /f /t /im TabTip.exe
+taskkill /f /t /im textinputhost.exe
+taskkill /f /t /im TiWorker.exe
+taskkill /f /t /im TrustedInstaller.exe
+taskkill /f /t /im uhssvc.exe
+taskkill /f /t /im vds.exe
+taskkill /f /t /im video.ui.exe
+taskkill /f /t /im vssadmin.exe
+taskkill /f /t /im VSSVC.exe
+taskkill /f /t /im werfault.exe
+taskkill /f /t /im WidgetService.exe
+taskkill /f /t /im Widgets.exe
+taskkill /f /t /im WindowsPackageManagerServer.exe
+taskkill /f /t /im winstore.app.exe
+taskkill /f /t /im WmiApSrv.exe
+taskkill /f /t /im WmiPrvSE.exe
+taskkill /f /t /im WUDFHost.exe
+taskkill /f /t /im yourphone.exe
 sc start "SysMain"
+::sc stop "SysMain"
 sc config "SysMain" start= auto
+::sc config "SysMain" start= disabled
 sc start "TabletInputService"
 sc config "TabletInputService" start= auto
 sc config "wlidsvc" start= auto
@@ -245,96 +245,115 @@ sc config WSAIFabricSvc start= disabled
 ie4uinit.exe -ClearIconCache
 powercfg.exe hibernate off
 reg delete "HKEY_CURRENT_USER\Software\Spoon" /f
-::bcdedit /deletevalue useplatformclock
-::bcdedit /deletevalue disabledynamictick
-::bcdedit /deletevalue useplatformtick
-::bcdedit /deletevalue uselegacyapicmode
-::bcdedit /deletevalue x2apicpolicy
-::bcdedit /deletevalue tscsyncpolicy
-::bcdedit /set hypervisorlaunchtype auto
-::bcdedit /deletevalue usephysicaldestination
-::bcdedit /deletevalue usefirmwarepcisettings
-::bcdedit /deletevalue tpmbootentropy
-::bcdedit /deletevalue bootux
-::bcdedit /deletevalue vsmlaunchtype
-::bcdedit /deletevalue vm
-::bcdedit /deletevalue quietboot
-::bcdedit /deletevalue ems
-::bcdedit /deletevalue bootems
-::bcdedit /deletevalue debug
-::bcdedit /deletevalue integrityservices
-::bcdedit /deletevalue disableelamdrivers
-::bcdedit /deletevalue isolatedcontext
-::bcdedit /deletevalue allowedinmemorysettings
-::bcdedit /deletevalue numproc
-::bcdedit /deletevalue maxproc
-::bcdedit /deletevalue configaccesspolicy
-::bcdedit /deletevalue MSI
-::bcdedit /deletevalue onecpu
-::bcdedit /deletevalue firstmegabytepolicy
-::bcdedit /deletevalue avoidlowmemory
-::bcdedit /deletevalue nolowmem
-::bcdedit /deletevalue linearaddress57
-::bcdedit /deletevalue increaseuserva
-bcdedit /set useplatformclock false
 bcdedit /set disabledynamictick Yes
 ::bcdedit /set disabledynamictick No
-::bcdedit /set useplatformtick No
-bcdedit /set useplatformtick Yes
-bcdedit /set uselegacyapicmode no
-::bcdedit /set x2apicpolicy Disable
-bcdedit /set x2apicpolicy Enable
+::bcdedit /deletevalue disabledynamictick
 bcdedit /set hypervisorlaunchtype off
-::bcdedit /set tscsyncpolicy legacy
+::bcdedit /set hypervisorlaunchtype auto
 bcdedit /set tscsyncpolicy enhanced
-bcdedit /set usephysicaldestination No
-bcdedit /set usefirmwarepcisettings No
-bcdedit /set tpmbootentropy ForceDisable
-bcdedit /set bootux Disabled
-bcdedit /set vsmlaunchtype off
-bcdedit /set vm no
-bcdedit /set quietboot yes
-bcdedit /set ems No
-bcdedit /set bootems No
-bcdedit /set debug No
-bcdedit /set integrityservices disable
-bcdedit /set disableelamdrivers Yes
-bcdedit /set isolatedcontext No
+::bcdedit /set tscsyncpolicy legacy
+::bcdedit /deletevalue tscsyncpolicy
+bcdedit /set uselegacyapicmode No
+::bcdedit /deletevalue uselegacyapicmode
+bcdedit /set useplatformclock false
+::bcdedit /deletevalue useplatformclock
+bcdedit /set useplatformtick Yes
+::bcdedit /set useplatformtick No
+::bcdedit /deletevalue useplatformtick
+bcdedit /set x2apicpolicy Enable
+::bcdedit /set x2apicpolicy Disable
+::bcdedit /deletevalue x2apicpolicy
 bcdedit /set allowedinmemorysettings 0x0
-bcdedit /set numproc 16
-bcdedit /set maxproc yes
-bcdedit /set configaccesspolicy Default
-bcdedit /set MSI Default
-bcdedit /set onecpu No
-bcdedit /set firstmegabytepolicy UseAll
+::bcdedit /deletevalue allowedinmemorysettings
 bcdedit /set avoidlowmemory 0x8000000
-bcdedit /set nolowmem Yes
-bcdedit /set linearaddress57 OptOut
+::bcdedit /deletevalue avoidlowmemory
+bcdedit /set bootems No
+::bcdedit /deletevalue bootems
+bcdedit /set bootux Disabled
+::bcdedit /deletevalue bootux
+bcdedit /set configaccesspolicy Default
+::bcdedit /deletevalue configaccesspolicy
+bcdedit /set debug No
+::bcdedit /deletevalue debug
+bcdedit /set disableelamdrivers Yes
+::bcdedit /deletevalue disableelamdrivers
+bcdedit /set ems No
+::bcdedit /deletevalue ems
+bcdedit /set firstmegabytepolicy UseAll
+::bcdedit /deletevalue firstmegabytepolicy
 bcdedit /set increaseuserva 268435328
+::bcdedit /deletevalue increaseuserva
+bcdedit /set integrityservices disable
+::bcdedit /deletevalue integrityservices
+bcdedit /set isolatedcontext No
+::bcdedit /deletevalue isolatedcontext
+bcdedit /set linearaddress57 OptOut
+::bcdedit /deletevalue linearaddress57
+bcdedit /set maxproc yes
+::bcdedit /deletevalue maxproc
+bcdedit /set MSI Default
+::bcdedit /deletevalue MSI
+bcdedit /set nolowmem Yes
+::bcdedit /deletevalue nolowmem
+bcdedit /set numproc 16
+::bcdedit /deletevalue numproc
+bcdedit /set onecpu No
+::bcdedit /deletevalue onecpu
+bcdedit /set quietboot yes
+::bcdedit /deletevalue quietboot
+bcdedit /set tpmbootentropy ForceDisable
+::bcdedit /deletevalue tpmbootentropy
+bcdedit /set usefirmwarepcisettings No
+::bcdedit /deletevalue usefirmwarepcisettings
+bcdedit /set usephysicaldestination No
+::bcdedit /deletevalue usephysicaldestination
+bcdedit /set vm no
+::bcdedit /deletevalue vm
+bcdedit /set vsmlaunchtype off
+::bcdedit /deletevalue vsmlaunchtype
+bcdedit /bootdebug Off
+bcdedit /bootems Off
+bcdedit /debug Off
+bcdedit /ems Off
+bcdedit /set bootdebug Off
+bcdedit /set bootlog No
+bcdedit /set bootmenupolicy Legacy
+bcdedit /set debugstart Disable
+bcdedit /set extendedinput Yes
+bcdedit /set forcefipscrypto No
+bcdedit /set {globalsettings} custom:16000067 true
+bcdedit /set graphicsmodedisabled No
+bcdedit /set halbreakpoint No
+bcdedit /set highestmode Yes
+bcdedit /set noumex Yes
+bcdedit /set pae ForceEnable
+bcdedit /set sos On
+bcdedit /timeout 0
 %windir%\system32\lodctr /R
 %windir%\sysWOW64\lodctr /R
 lodctr /e:PerfOS
 taskkill /f /t /im OfficeClickToRun.exe
 taskkill /f /t /im ShellHost.exe
-wmic process where name="OfficeClickToRun.exe" CALL terminate
 wmic process where name="Adobe Crash Processor.exe" CALL terminate
+wmic process where name="MoUsoCoreWorker.exe" CALL terminate
 wmic process where name="MpDefenderCoreService.exe" CALL terminate
+wmic process where name="OfficeClickToRun.exe" CALL terminate
 wmic process where name="taskhostw.exe" CALL terminate
 wmic process where name="WmiPrvSvc.exe" CALL terminate
-wmic process where name="MoUsoCoreWorker.exe" CALL terminate
+wmic process where name="dwm.exe" CALL setpriority 32768
 ::wmic process where name="dwm.exe" CALL setpriority 64
 wmic process where name="dllhost.exe" CALL setpriority 64
-wmic process where name="winlogon.exe" CALL setpriority 64
 wmic process where name="fontdrvhost.exe" CALL setpriority 64
+wmic process where name="winlogon.exe" CALL setpriority 64
 taskkill /f /t /im MoUsoCoreWorker.exe
-taskkill /f /t /im WMIADAP.exe
-taskkill /f /t /im UserOOBEBroker.exe
 taskkill /f /t /im RuntimeBroker.exe
+taskkill /f /t /im UserOOBEBroker.exe
+taskkill /f /t /im WMIADAP.exe
 bcdedit /set nx AlwaysOff
 powershell -Command "Set-ProcessMitigation -System -Disable DEP,EmulateAtlThunks,ForceRelocateImages,RequireInfo,BottomUp,HighEntropy,StrictHandle,DisableWin32kSystemCalls,AuditSystemCall,DisableExtensionPoints,BlockDynamicCode,AllowThreadsToOptOut,AuditDynamicCode,CFG,SuppressExports,StrictCFG,MicrosoftSignedOnly,AllowStoreSignedBinaries,AuditMicrosoftSigned,AuditStoreSigned,EnforceModuleDependencySigning,DisableNonSystemFonts,AuditFont,BlockRemoteImageLoads,BlockLowLabelImageLoads,PreferSystem32,AuditRemoteImageLoads,AuditLowLabelImageLoads,AuditPreferSystem32,SEHOP,AuditSEHOP,SEHOPTelemetry,TerminateOnError"
 bcdedit /deletevalue nointegritychecks
 bcdedit /deletevalue loadoptions
-bcdedit /debug off
+::bcdedit /debug off
 ::bcdedit /deletevalue nx
 taskkill /f /t /im CompPkgSrv.exe
 reg delete "HKLM\SYSTEM\CurrentControlSet\Enum\DISPLAY\GSM60B2\5&2adb58f6&0&UID37124\Device Parameters" /v EDID /f
@@ -344,12 +363,20 @@ taskkill /f /t /im SearchProtocolHost.exe
 taskkill /f /t /im SearchIndexer.exe
 taskkill /f /t /im SearchFilterHost.exe
 taskkill /f /t /im SearchApp.exe
-taskkill /f /t /im node.exe
-taskkill /f /t /im powershell.exe
 powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-PnpDevice -FriendlyName 'High precision event timer' | Disable-PnpDevice -Confirm:$false"
 powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-PnpDevice -FriendlyName 'Microsoft Device Association Root Enumerator' -ErrorAction SilentlyContinue | Disable-PnpDevice -Confirm:$false -ErrorAction SilentlyContinue"
 powershell -NoProfile -ExecutionPolicy Bypass -Command "$devices = Get-PnpDevice | Where-Object {$_.FriendlyName -like '*Device Association*'}; foreach ($device in $devices) { try { pnputil /remove-device $device.InstanceId; Start-Sleep -Seconds 1; pnputil /remove-device $device.InstanceId /force; } catch {} }"
+::powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-PnpDevice -FriendlyName 'Microsoft GS Wavetable Synth' | Disable-PnpDevice -Confirm:$false"
+::powershell -NoProfile -ExecutionPolicy Bypass -Command "$device = Get-PnpDevice | Where-Object {$_.FriendlyName -like '*RRAS*'}; if ($device) { $device | Disable-PnpDevice -Confirm:$false }"
+::powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-PnpDevice -FriendlyName 'Composite Bus Enumerator' | Disable-PnpDevice -Confirm:$false"
+::powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-PnpDevice -FriendlyName 'Microsoft Virtual Drive Enumerator' | Disable-PnpDevice -Confirm:$false"
+::powershell -NoProfile -ExecutionPolicy Bypass -Command "$device = Get-PnpDevice | Where-Object {$_.FriendlyName -like '*Remote Desktop*'}; if ($device) { try { $device | Disable-PnpDevice -Confirm:$false -ErrorAction SilentlyContinue } catch {} }"
+::powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-PnpDevice -FriendlyName 'UMBus Root Bus Enumerator' | Disable-PnpDevice -Confirm:$false"
+::powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-PnpDevice -FriendlyName 'NDIS Virtual Network Adapter Enumerator' | Disable-PnpDevice -Confirm:$false"
+::powershell -NoProfile -ExecutionPolicy Bypass -Command "$device = Get-PnpDevice | Where-Object {$_.FriendlyName -like '*AMD PSP*' -or $_.FriendlyName -like '*Platform Security Processor*'}; if ($device) { try { $device | Disable-PnpDevice -Confirm:$false -ErrorAction SilentlyContinue } catch {} }"
 timeout /t 8 /nobreak
+taskkill /f /t /im node.exe
+taskkill /f /t /im powershell.exe
 taskkill /f /t /im conhost.exe
 taskkill /f /t /im cmd.exe
 exit
