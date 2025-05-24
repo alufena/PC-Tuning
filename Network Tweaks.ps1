33ad8db8510e9c395a6ca2029140f18e4dd2ecd0
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-05-24 03:30:27 -0300

    Update Network Tweaks.ps1

--- Network Tweaks.ps1
+++ Network Tweaks.ps1
@@ -4,26 +4,32 @@
 netsh interface tcp set supplemental Internet congestionprovider=ctcp
 netsh int tcp set global dca=enabled
 netsh int tcp set global netdma=enabled
-netsh int tcp set global ecncapability=enabled
+netsh int tcp set global ecncapability=disabled
 netsh int tcp set global fastopen=enabled
 netsh int tcp set global fastopenfallback=enabled
 netsh int tcp set global rss=enabled
-netsh int tcp set global rsc=enabled
+netsh int tcp set global rsc=disabled
 netsh int tcp set global pacingprofile=always
 netsh int tcp set global hystart=enabled
-netsh int tcp set global autotuninglevel=experimental
+netsh int tcp set global autotuninglevel=disabled
 netsh int tcp set supplemental internet enablecwndrestart=enabled
 netsh int tcp set supplemental internet congestionprovider=ctcp
 netsh int tcp set security mpp=enabled
-netsh int ip set global neighborcachelimit=4096 taskoffload=enabled
+netsh int ip set global neighborcachelimit=4096 taskoffload=disabled
+NETSH interface teredo set state disabled
+NETSH interface 6to4 set state disabled
+NETSH int ip set global loopbackworkercount=16
+NETSH int tcp set global nonsackrttresiliency=disabled
+NETSH int tcp set global timestamps=disabled
+NETSH int tcp set security profiles=disabled
+NETSH int tcp set global initialRto=3000
+NETSH int tcp set global maxsynretransmissions=2
 netsh int isatap set state disabled
 
 # ------------------------------
 # NETTCPSETTINGS (Modern API)
 # ------------------------------
-Set-NetTCPSetting -SettingName * -AutoTuningLevelLocal Experimental -ScalingHeuristics Disabled
-# Note: The line below caused an error in previous attempts because InitialCongestionWindow is read-only on modern Windows versions.
-# Set-NetTCPSetting -SettingName * -InitialCongestionWindow 10 -InitialRto 3000 -Timestamps Disabled -MemoryPressureProtection Disabled -NonSackRttResiliency Disabled -MaxSynRetransmissions 2
+Set-NetTCPSetting -SettingName * -AutoTuningLevelLocal Disable -ScalingHeuristics Disabled
 
 # ------------------------------
 # NETWORK ADAPTER SETTINGS
@@ -32,10 +38,10 @@ Disable-NetAdapterPowerManagement *
 Disable-NetAdapterLso -Name *
 Disable-NetAdapterBinding -Name * -ComponentID ms_pacer
 
-# If you share files on the network, DO NOT disable the client below
+# Se você compartilha arquivos na rede, NÃO desative o cliente abaixo
 Disable-NetAdapterBinding -Name * -ComponentID ms_msclient
 
-# Offload Settings — some enabled for network performance, others disabled for stability
+# Offload Settings — alguns ativados para performance de rede, outros desativados por estabilidade
 Set-NetOffloadGlobalSetting -PacketCoalescingFilter disabled -Chimney Disabled -Taskoffload Enabled -ReceiveSideScaling Enabled -ReceiveSegmentCoalescing Disabled
 
 Set-NetAdapterRdma -Name "*" -Enabled $True
@@ -67,7 +73,7 @@ function Set-RegKey {
     }
 }
 
-# Disable Nagle and ACK delay
+# Desativar Nagle e ACK delay
 $tcpipPath = "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces"
 $tcpInterfaces = Get-ChildItem -Path $tcpipPath
 foreach ($interface in $tcpInterfaces) {
@@ -76,7 +82,7 @@ foreach ($interface in $tcpInterfaces) {
     Set-RegKey -path $interface.PSPath -name "TcpDelAckTicks" -value 0
 }
 
-# NetBIOS: Disable for less local overhead
+# NetBIOS: Desativar para menor overhead local
 $netbtPath = "HKLM:\SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
 $interfaces = Get-ChildItem -Path $netbtPath
 foreach ($interface in $interfaces) {
@@ -102,7 +108,7 @@ foreach ($interface in $networkInterfaces) {
     # Interrupt Moderation
     Set-RegKey -path $interface.PSPath -name "*InterruptModeration" -value "0" -type "String"
 
-    # Green Ethernet and power-saving
+    # Green Ethernet e power-saving
     Set-RegKey -path $interface.PSPath -name "*EEE" -value "0" -type "String"
     Set-RegKey -path $interface.PSPath -name "AdvancedEEE" -value "0" -type "String"
     Set-RegKey -path $interface.PSPath -name "AutoPowerSaveModeEnabled" -value "0" -type "String"
@@ -111,3 +117,8 @@ foreach ($interface in $networkInterfaces) {
     Set-RegKey -path $interface.PSPath -name "PowerSavingMode" -value "0" -type "String"
     Set-RegKey -path $interface.PSPath -name "ULPMode" -value "0" -type "String"
 }
+
+# ------------------------------
+# OPCIONAL: ATIVA PERFIS DCTCP
+# ------------------------------
+# netsh int tcp set supplemental internet congestionprovider=dctcp
