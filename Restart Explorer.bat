076975b9fe61b4e88143f668117c9bd4cd517071
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-06-02 20:08:58 -0300

    Update Restart Explorer.bat

--- Restart Explorer.bat
+++ Restart Explorer.bat
@@ -1,4 +1,7 @@
 taskkill /f /im explorer.exe
+taskkill /f /im dwm.exe
 taskkill /f /im NVDisplay.Container.exe
+taskkill /f /t /im firefox.exe
 timeout /t 1 /nobreak > nul
 start explorer.exe
+start firefox.exe
