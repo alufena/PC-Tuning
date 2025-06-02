d9afe22342de74f1baaeeddcf9d0fa69ad9720c8
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-06-02 20:11:01 -0300

    Update Restart Explorer.bat

--- Restart Explorer.bat
+++ Restart Explorer.bat
@@ -1,7 +1,8 @@
+tasklist | find "firefox.exe" > nul && set restart_firefox=1
 taskkill /f /im explorer.exe
 taskkill /f /im dwm.exe
 taskkill /f /im NVDisplay.Container.exe
 taskkill /f /t /im firefox.exe
 timeout /t 1 /nobreak > nul
 start explorer.exe
-start firefox.exe
+if defined restart_firefox start firefox.exe
