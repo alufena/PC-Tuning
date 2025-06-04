259f4a72fbb24c394168478e923fe346196bff9c
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-06-04 16:48:21 -0300

    Update Disable Scheduler Events.bat

--- Disable Scheduler Events.bat
+++ Disable Scheduler Events.bat
@@ -1,23 +1,49 @@
-schtasks /change /tn "microsoft\office\office clicktorun service monitor" /disable
+schtasks /change /tn "EqualizerAPOUpdateChecker" /disable
+schtasks /change /tn "MicrosoftEdgeUpdateTaskMachineCore" /disable
+schtasks /change /tn "MicrosoftEdgeUpdateTaskMachineUA" /disable
+schtasks /change /TN "\Microsoft\Office\Office 15 Subscription Heartbeat" /disable
+schtasks /change /tn "Microsoft\Office\Office Automatic Updates 2.0" /disable
 schtasks /change /tn "Microsoft\Office\Office Automatic Updates" /disable
-schtasks /change /tn "microsoft\windows\.net framework\.net framework ngen v4.0.30319 64 critical" /disable
-schtasks /change /tn "microsoft\windows\.net framework\.net framework ngen v4.0.30319 64" /disable
-schtasks /change /tn "microsoft\windows\.net framework\.net framework ngen v4.0.30319 critical" /disable
-schtasks /change /tn "microsoft\windows\.net framework\.net framework ngen v4.0.30319" /disable
-schtasks /change /tn "microsoft\windows\application experience\microsoft compatibility appraiser exp" /disable
+schtasks /change /tn "microsoft\office\office clicktorun service monitor" /disable
+schtasks /change /tn "Microsoft\Office\Office Feature Updates" /disable
+schtasks /change /tn "Microsoft\Office\Office Feature Updates Logon" /disable
+schtasks /change /tn "\Microsoft\Office\OfficeTelemetryAgentFallBack2016" /disable
+schtasks /change /tn "microsoft\office\officeTelemetryAgentFallBack" /disable
+schtasks /change /tn "\Microsoft\Office\OfficeTelemetryAgentLogOn2016" /disable
+schtasks /change /tn "microsoft\office\officeTelemetryAgentLogOn" /disable
+schtasks /change /tn "Microsoft\VisualStudio\Updates\BackgroundDownload" /disable
+schtasks /change /tn "Microsoft\Windows\Active Directory Rights Management Services Client\AD RMS Rights Policy Template Management (Manual)" /disable
+schtasks /change /tn "\Microsoft\Windows\AppID\SmartScreenSpecific" /disable
+schtasks /change /tn "\Microsoft\Windows\Application Experience\AitAgent" /disable
 schtasks /change /tn "microsoft\windows\application experience\microsoft compatibility appraiser" /disable
+schtasks /change /tn "microsoft\windows\application experience\microsoft compatibility appraiser exp" /disable
 schtasks /change /tn "microsoft\windows\application experience\pcapatchdbtask" /disable
 schtasks /change /tn "microsoft\windows\application experience\programdataupdater" /disable
 schtasks /change /tn "microsoft\windows\application experience\sdbinstmergedbtask" /disable
 schtasks /change /tn "microsoft\windows\application experience\startupapptask" /disable
 schtasks /change /tn "microsoft\windows\appxdeploymentclient\ucpd velocity" /disable
 schtasks /change /tn "microsoft\windows\autochk\proxy" /disable
+schtasks /change /tn "\Microsoft\Windows\CloudExperienceHost\CreateObjectTask" /disable
+schtasks /change /tn "\Microsoft\Windows\Customer Experience Improvement Program\BthSQM" /disable
 schtasks /change /tn "microsoft\windows\customer experience improvement program\consolidator" /disable
 schtasks /change /tn "microsoft\windows\customer experience improvement program\kernelceiptask" /disable
+schtasks /change /tn "\Microsoft\Windows\Customer Experience Improvement Program\Uploader" /disable
 schtasks /change /tn "microsoft\windows\customer experience improvement program\usbceip" /disable
 schtasks /change /tn "microsoft\windows\defrag\scheduleddefrag" /disable
-schtasks /change /tn "microsoft\windows\device information\device user" /disable
+schtasks /change /tn "Microsoft\Windows\DeviceDirectoryClient\HandleCommand" /disable
+schtasks /change /tn "Microsoft\Windows\DeviceDirectoryClient\HandleWnsCommand" /disable
+schtasks /change /tn "Microsoft\Windows\DeviceDirectoryClient\IntegrityCheck" /disable
+schtasks /change /tn "Microsoft\Windows\DeviceDirectoryClient\LocateCommandUserSession" /disable
+schtasks /change /tn "Microsoft\Windows\DeviceDirectoryClient\RegisterDeviceAccountChange" /disable
+schtasks /change /tn "Microsoft\Windows\DeviceDirectoryClient\RegisterDeviceLocationRightsChange" /disable
+schtasks /change /tn "Microsoft\Windows\DeviceDirectoryClient\RegisterDevicePeriodic24" /disable
+schtasks /change /tn "Microsoft\Windows\DeviceDirectoryClient\RegisterDevicePolicyChange" /disable
+schtasks /change /tn "Microsoft\Windows\DeviceDirectoryClient\RegisterDeviceProtectionStateChanged" /disable
+schtasks /change /tn "Microsoft\Windows\DeviceDirectoryClient\RegisterDeviceSettingChange" /disable
+schtasks /change /tn "Microsoft\Windows\DeviceDirectoryClient\RegisterUserDevice" /disable
 schtasks /change /tn "microsoft\windows\device information\device" /disable
+schtasks /change /tn "microsoft\windows\device information\device user" /disable
+schtasks /change /tn "Microsoft\Windows\Device Setup\Metadata Refresh" /disable
 schtasks /change /tn "microsoft\windows\diagnosis\recommendedtroubleshootingscanner" /disable
 schtasks /change /tn "microsoft\windows\diagnosis\scheduled" /disable
 schtasks /change /tn "microsoft\windows\diskcleanup\silentcleanup" /disable
@@ -27,8 +53,11 @@ schtasks /change /tn "microsoft\windows\diskfootprint\diagnostics" /disable
 schtasks /change /tn "microsoft\windows\diskfootprint\storagesense" /disable
 schtasks /change /tn "microsoft\windows\dusm\dusmtask" /disable
 schtasks /change /tn "microsoft\windows\enterprisemgmt\mdmmaintenencetask" /disable
+schtasks /Change /TN "Microsoft\Windows\ErrorDetails\EnableErrorDetailsUpdate" /Disable
+schtasks /Change /TN "Microsoft\Windows\ExploitGuard\ExploitGuard MDM policy Refresh" /Disable
 schtasks /change /tn "microsoft\windows\feedback\siuf\dmclient" /disable
 schtasks /change /tn "microsoft\windows\feedback\siuf\dmclientonscenariodownload" /disable
+schtasks /change /tn "Microsoft\Windows\File Classification Infrastructure\Property Definition Sync" /disable
 schtasks /change /tn "microsoft\windows\filehistory\file history (maintenance mode)" /disable
 schtasks /change /tn "microsoft\windows\flighting\featureconfig\reconcilefeatures" /disable
 schtasks /change /tn "microsoft\windows\flighting\featureconfig\usagedataflushing" /disable
@@ -39,6 +68,9 @@ schtasks /change /tn "microsoft\windows\input\inputsettingsrestoredataavailable"
 schtasks /change /tn "microsoft\windows\input\localusersyncdataavailable" /disable
 schtasks /change /tn "microsoft\windows\input\mousesyncdataavailable" /disable
 schtasks /change /tn "microsoft\windows\input\pensyncdataavailable" /disable
+schtasks /Change /TN "Microsoft\Windows\Input\RemoteMouseSyncDataAvailable" /Disable
+schtasks /Change /TN "Microsoft\Windows\Input\RemotePenSyncDataAvailable" /Disable
+schtasks /Change /TN "Microsoft\Windows\Input\RemoteTouchpadSyncDataAvailable" /Disable
 schtasks /change /tn "microsoft\windows\input\syncpensettings" /disable
 schtasks /change /tn "microsoft\windows\input\touchpadsyncdataavailable" /disable
 schtasks /change /tn "microsoft\windows\international\synchronize language settings" /disable
@@ -48,6 +80,7 @@ schtasks /change /tn "microsoft\windows\languagecomponentsinstaller\uninstallati
 schtasks /change /tn "microsoft\windows\license manager\tempsignedlicenseexchange" /disable
 schtasks /change /tn "microsoft\windows\location\windowsactiondialog" /disable
 schtasks /change /tn "microsoft\windows\maintenance\winsat" /disable
+schtasks /Change /TN "Microsoft\Windows\Management\Connectivity\ESIMPM" /Disable
 schtasks /change /tn "microsoft\windows\management\provisioning\cellular" /disable
 schtasks /change /tn "microsoft\windows\management\provisioning\logon" /disable
 schtasks /change /tn "microsoft\windows\maps\mapstoasttask" /disable
@@ -56,8 +89,13 @@ schtasks /change /tn "microsoft\windows\memorydiagnostic\processmemorydiagnostic
 schtasks /change /tn "microsoft\windows\memorydiagnostic\runfullmemorydiagnostic" /disable
 schtasks /change /tn "microsoft\windows\mui\lpremove" /disable
 schtasks /change /tn "microsoft\windows\multimedia\systemsoundsservice" /disable
+schtasks /change /tn "microsoft\windows\.net framework\.net framework ngen v4.0.30319 64 critical" /disable
+schtasks /change /tn "microsoft\windows\.net framework\.net framework ngen v4.0.30319 64" /disable
+schtasks /change /tn "microsoft\windows\.net framework\.net framework ngen v4.0.30319 critical" /disable
+schtasks /change /tn "microsoft\windows\.net framework\.net framework ngen v4.0.30319" /disable
 schtasks /change /tn "microsoft\windows\nettrace\gathernetworkinfo" /disable
 schtasks /change /tn "microsoft\windows\network connectivity status indicator\ncsiidentifyuserproxies" /disable
+schtasks /Change /TN "Microsoft\Windows\NetworkExperimentation" /Disable
 schtasks /change /tn "microsoft\windows\nlasvc\wifitask" /disable
 schtasks /change /tn "microsoft\windows\performancetrace\requesttrace" /disable
 schtasks /change /tn "microsoft\windows\pi\sqm-tasks" /disable
@@ -72,17 +110,22 @@ schtasks /change /tn "microsoft\windows\remoteassistance\remoteassistancetask" /
 schtasks /change /tn "microsoft\windows\retaildemo\cleanupofflinecontent" /disable
 schtasks /change /tn "microsoft\windows\servicing\startcomponentcleanup" /disable
 schtasks /change /tn "microsoft\windows\settingsync\networkstatechangetask" /disable
+schtasks /change /tn "Microsoft\Windows\SettingSync\NetworkStateChangeTask" /disable
 schtasks /change /tn "microsoft\windows\setup\setupcleanuptask" /disable
 schtasks /change /tn "microsoft\windows\setup\snapshotcleanuptask" /disable
 schtasks /change /tn "microsoft\windows\shell\familysafetymonitor" /disable
 schtasks /change /tn "microsoft\windows\shell\familysafetyrefreshtask" /disable
+schtasks /change /tn "\Microsoft\Windows\Shell\FamilySafetyUpload" /disable
 schtasks /change /tn "microsoft\windows\shell\themessyncedimagedownload" /disable
+schtasks /change /tn "Microsoft\Windows\SoftwareProtectionPlatform\SvcRestartTask" /disable
 schtasks /change /tn "microsoft\windows\spaceport\spaceagenttask" /disable
 schtasks /change /tn "microsoft\windows\spaceport\spacemanagertask" /disable
 schtasks /change /tn "microsoft\windows\speech\speechmodeldownloadtask" /disable
 schtasks /change /tn "microsoft\windows\storage tiers management\storage tiers management initialization" /disable
 schtasks /change /tn "microsoft\windows\sustainability\powergridforecasttask" /disable
 schtasks /change /tn "microsoft\windows\sustainability\sustainabilitytelemetry" /disable
+schtasks /change /tn "Microsoft\Windows\Sysmain\HybridDriveCachePrepopulate" /disable
+schtasks /change /tn "Microsoft\Windows\Sysmain\HybridDriveCacheRebalance" /disable
 schtasks /change /tn "microsoft\windows\sysmain\respristaticdbsync" /disable
 schtasks /change /tn "microsoft\windows\sysmain\wsswapassessmenttask" /disable
 schtasks /change /tn "microsoft\windows\task manager\interactive" /disable
@@ -92,13 +135,18 @@ schtasks /change /tn "microsoft\windows\time synchronization\synchronizetime" /d
 schtasks /change /tn "microsoft\windows\time zone\synchronizetimezone" /disable
 schtasks /change /tn "microsoft\windows\tpm\tpm-hascertretr" /disable
 schtasks /change /tn "microsoft\windows\tpm\tpm-maintenance" /disable
+schtasks /change /tn "Microsoft\Windows\UNP\RunUpdateNotificationMgr" /disable
 schtasks /change /tn "microsoft\windows\upnp\upnphostconfig" /disable
 schtasks /change /tn "microsoft\windows\user profile service\hiveuploadtask" /disable
 schtasks /change /tn "microsoft\windows\wcm\wifitask" /disable
 schtasks /change /tn "microsoft\windows\wdi\resolutionhost" /disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable
 schtasks /change /tn "microsoft\windows\windows error reporting\queuereporting" /disable
-schtasks /Change /TN "Microsoft\Windows\ErrorDetails\EnableErrorDetailsUpdate" /Disable
 schtasks /change /tn "microsoft\windows\windows filtering platform\bfeonservicestarttypechange" /disable
+schtasks /change /TN "\Microsoft\Windows\WindowsUpdate\Automatic App Update" /disable
 schtasks /change /tn "microsoft\windows\windowsupdate\scheduled start" /disable
 schtasks /change /tn "microsoft\windows\wlansvc\cdssync" /disable
 schtasks /change /tn "microsoft\windows\wlansvc\moprofilemanagement" /disable
@@ -109,36 +157,10 @@ schtasks /change /tn "microsoft\windows\work folders\work folders maintenance wo
 schtasks /change /tn "microsoft\windows\workplace join\automatic-device-join" /disable
 schtasks /change /tn "microsoft\windows\wwansvc\notificationtask" /disable
 schtasks /change /tn "microsoft\windows\wwansvc\oobediscovery" /disable
-schtasks /change /tn "microsoft\office\officeTelemetryAgentFallBack" /disable
-schtasks /change /tn "microsoft\office\officeTelemetryAgentLogOn" /disable
 schtasks /Change /TN "Microsoft\XblGameSave\XblGameSaveTask" /Disable
-schtasks /Change /TN "Microsoft\Windows\ExploitGuard\ExploitGuard MDM policy Refresh" /Disable
-schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable
-schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable
-schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable
-schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable
-schtasks /Change /TN "Microsoft\Windows\NetworkExperimentation" /Disable
-schtasks /Change /TN "Microsoft\Windows\Management\Connectivity\ESIMPM" /Disable
-schtasks /Change /TN "Microsoft\Windows\Input\RemoteMouseSyncDataAvailable" /Disable
-schtasks /Change /TN "Microsoft\Windows\Input\RemotePenSyncDataAvailable" /Disable
-schtasks /Change /TN "Microsoft\Windows\Input\RemoteTouchpadSyncDataAvailable" /Disable
-schtasks /change /tn "EqualizerAPOUpdateChecker" /disable
-schtasks /change /tn "MicrosoftEdgeUpdateTaskMachineCore" /disable
-schtasks /change /tn "MicrosoftEdgeUpdateTaskMachineUA" /disable
+schtasks /change /tn "Mozilla\Firefox Background Update 308046B0AF4A39CB" /disable
 schtasks /change /tn "MSIAfterburner" /disable
-schtasks /change /tn "Microsoft\Windows\SoftwareProtectionPlatform\SvcRestartTask" /disable
-schtasks /change /tn "Microsoft\Windows\SettingSync\NetworkStateChangeTask" /disable
-schtasks /change /tn "Microsoft\VisualStudio\Updates\BackgroundDownload" /disable
-schtasks /change /tn "\Microsoft\Windows\Shell\FamilySafetyUpload" /disable
-schtasks /change /tn "\Microsoft\Windows\Customer Experience Improvement Program\BthSQM" /disable
-schtasks /change /tn "\Microsoft\Windows\Customer Experience Improvement Program\Uploader" /disable
-schtasks /change /tn "\Microsoft\Windows\Application Experience\AitAgent" /disable
-schtasks /change /tn "\Microsoft\Windows\CloudExperienceHost\CreateObjectTask" /disable
-schtasks /change /tn "\Microsoft\Windows\AppID\SmartScreenSpecific" /disable
-schtasks /change /tn "\Microsoft\Office\OfficeTelemetryAgentFallBack2016" /disable
-schtasks /change /tn "\Microsoft\Office\OfficeTelemetryAgentLogOn2016" /disable
-schtasks /change /TN "\Microsoft\Office\Office 15 Subscription Heartbeat" /disable
-schtasks /change /TN "\Microsoft\Windows\WindowsUpdate\Automatic App Update" /disable
+schtasks /delete /tn "Microsoft\Windows\RetailDemo\CleanupOfflineContent" /f
 powershell -Command "Get-ScheduledTask | Where-Object { $_.TaskName -like 'MEGAsync Update Task*' } | ForEach-Object { Disable-ScheduledTask -TaskName $_.TaskName -TaskPath $_.TaskPath }"
 powershell -Command "Get-ScheduledTask | Where-Object { $_.TaskName -like 'GoogleUpdaterTaskSystem*' } | ForEach-Object { Disable-ScheduledTask -TaskName $_.TaskName -TaskPath $_.TaskPath }"
 powershell -Command "Get-ScheduledTask | Where-Object { $_.TaskName -like 'Firefox Default Browser Agent*' } | ForEach-Object { Disable-ScheduledTask -TaskName $_.TaskName -TaskPath $_.TaskPath }"
