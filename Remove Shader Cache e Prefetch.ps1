57fbe458c7aa85939b4215684e5143969589cba7
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-06-05 17:23:08 -0300

    Update Remove Shader Cache e Prefetch.ps1

--- Remove Shader Cache e Prefetch.ps1
+++ Remove Shader Cache e Prefetch.ps1
@@ -1,16 +1,16 @@
-﻿$folders = @(
+$folders = @(
     "C:\Program Files (x86)\Steam\steamapps\common\No Man's Sky\GAMEDATA\SHADERCACHE",
     "C:\Program Files (x86)\Steam\steamapps\shadercache",
     "C:\Users\Administrator\AppData\Local\D3DSCache",
     "C:\Users\Administrator\AppData\LocalLow\NVIDIA\PerDriverVersion\DXCache",
     "C:\Users\Administrator\AppData\LocalLow\NVIDIA\PerDriverVersion\GLCache",
-    "C:\Users\Administrator\AppData\Local\NVIDIA\DXCache",
+    "C:\Users\Administrator\AppData\Local\NVIDIA\DX遵
     "C:\Users\Administrator\AppData\Local\NVIDIA\GLCache",
     "D:\SteamLibrary\steamapps\shadercache",
     "E:\SteamLibrary\steamapps\shadercache",
     "F:\SteamLibrary\steamapps\shadercache",
     "G:\SteamLibrary\steamapps\shadercache",
-	"C:\Windows\Prefetch",
+    "C:\Windows\Prefetch",
     "C:\Users\Administrator\AppData\Local\NVIDIA\VulkanCache",
     "C:\Users\Administrator\AppData\LocalLow\NVIDIA\PerDriverVersion\VkCache",
     "C:\ProgramData\NVIDIA\NvBackend\ApplicationOntology\cache",
@@ -25,8 +25,8 @@
 foreach ($folder in $folders) {
     if (Test-Path $folder) {
         Get-ChildItem $folder -Recurse -File | Remove-Item -Force
-        Write-Host "$folder - Arquivos deletados"
+        Write-Host "$folder - Files deleted"
     } else {
-        Write-Host "$folder - Pasta não encontrada"
+        Write-Host "$folder - Folder not found"
     }
 }
