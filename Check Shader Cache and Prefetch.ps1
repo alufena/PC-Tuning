ab966a48cb2fb8f3acdbecc16a0fdfb8bedfa65a
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-06-05 17:08:50 -0300

    Update Check Shader Cache and Prefetch.ps1

--- Check Shader Cache and Prefetch.ps1
+++ Check Shader Cache and Prefetch.ps1
@@ -1,4 +1,4 @@
-﻿$folders = @(
+$folders = @(
     "C:\Program Files (x86)\Steam\steamapps\common\No Man's Sky\GAMEDATA\SHADERCACHE",
     "C:\Program Files (x86)\Steam\steamapps\shadercache",
     "C:\Users\Administrator\AppData\Local\D3DSCache",
@@ -10,7 +10,7 @@
     "E:\SteamLibrary\steamapps\shadercache",
     "F:\SteamLibrary\steamapps\shadercache",
     "G:\SteamLibrary\steamapps\shadercache",
-	"C:\Windows\Prefetch",
+    "C:\Windows\Prefetch",
     "C:\Users\Administrator\AppData\Local\NVIDIA\VulkanCache",
     "C:\Users\Administrator\AppData\LocalLow\NVIDIA\PerDriverVersion\VkCache",
     "C:\ProgramData\NVIDIA\NvBackend\ApplicationOntology\cache",
@@ -28,19 +28,20 @@ foreach ($folder in $folders) {
     if (Test-Path $folder) {
         $size = (Get-ChildItem $folder -Recurse -File | Measure-Object -Property Length -Sum).Sum
         $sizeMB = "{0:N2}" -f ($size / 1MB)
-        Write-Host "$folder - Tamanho: $sizeMB MB"
+        Write-Host "$folder - Size: $sizeMB MB"
         $totalSize += $size
     } else {
-        Write-Host "$folder - Pasta não encontrada"
+        Write-Host "$folder - Folder not found"
     }
 }
 
-# Calcula e exibe o tamanho total em diferentes unidades
+# Calculate and display total size in different units
 $totalSizeMB = "{0:N2}" -f ($totalSize / 1MB)
 $totalSizeGB = "{0:N2}" -f ($totalSize / 1GB)
 
 Write-Host ""
 Write-Host "============================================="
-Write-Host "TAMANHO TOTAL DE TODOS OS CACHES DE SHADER E PREFETCH:"
+Write-Host "TOTAL SIZE OF ALL SHADER AND PREFETCH CACHES:"
 Write-Host "$totalSizeMB MB ($totalSizeGB GB)"
 Write-Host "============================================="
+pause
