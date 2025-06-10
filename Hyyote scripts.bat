39065836d034f4ba0be56ada0d56b4c137817c91
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-06-09 22:12:50 -0300

    Update Hyyote scripts.bat

--- Hyyote scripts.bat
+++ Hyyote scripts.bat
@@ -120,4 +120,39 @@ for /f %%i in ('wmic path Win32_IDEController get PNPDeviceID ^| findstr /l "PCI
     reg add "HKLM\SYSTEM\CurrentControlSet\Enum\%%i\Device Parameters\Interrupt Management\Affinity Policy" /v "DevicePriority" /t REG_DWORD /d "0" /f
 )
 
+DISM > nul 2>&1 || echo error: administrator privileges required && pause && exit /b 1
+
+for %%a in (
+    "EnhancedPowerManagementEnabled"
+    "AllowIdleIrpInD3"
+    "EnableSelectiveSuspend"
+    "DeviceSelectiveSuspended"
+    "SelectiveSuspendEnabled"
+    "SelectiveSuspendOn"
+    "WaitWakeEnabled"
+    "D3ColdSupported"
+    "WdfDirectedPowerTransitionEnable"
+    "EnableIdlePowerManagement"
+    "IdleInWorkingState"
+) do (
+    echo info: configuring %%~a
+    for /f "delims=" %%b in ('reg query "HKLM\SYSTEM\CurrentControlSet\Enum" /s /f "%%~a" ^| findstr "HKEY"') do (
+        reg.exe add "%%b" /v "%%~a" /t REG_DWORD /d "0" /f > nul 2>&1
+    )
+)
+
+for %%a in (
+    "WakeEnabled" 
+    "WdkSelectiveSuspendEnable"
+) do (
+    echo info: configuring %%~a
+    for /f "delims=" %%b in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Class" /s /f "%%~a" ^| findstr "HKEY"') do (
+        reg.exe add "%%b" /v "%%~a" /t REG_DWORD /d "0" /f > nul 2>&1
+    )
+)
+
+echo info: done
+echo info: press any key to continue
+pause > nul 2>&1
+
 exit
