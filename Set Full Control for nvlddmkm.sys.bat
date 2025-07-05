7baf9a1ec147b5d4f5d51108716e740a32565e44
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-07-05 00:02:34 -0300

    Add files via upload

---
+++ Set Full Control for nvlddmkm.sys.bat
@@ -0,0 +1,15 @@
+@echo off
+set "BaseDir=%SystemRoot%\System32\DriverStore\FileRepository"
+
+for /d %%D in ("%BaseDir%\nv_dispi.inf_amd64_*") do (
+    if exist "%%D\nvlddmkm.sys" (
+        set "Target=%%D\nvlddmkm.sys"
+        goto :Apply
+    )
+)
+
+exit /b 1
+
+:Apply
+takeown /F "%Target%" /A
+icacls   "%Target%" /grant *S-1-5-32-544:F
