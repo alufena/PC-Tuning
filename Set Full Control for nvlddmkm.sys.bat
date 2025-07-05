9abfe21f8a88cca5ab222d85aa54e6a2c9e07aee
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-07-05 19:50:15 -0300

    Update Set Full Control for nvlddmkm.sys.bat

--- Set Full Control for nvlddmkm.sys.bat
+++ Set Full Control for nvlddmkm.sys.bat
@@ -1,18 +1,26 @@
 @echo off
+setlocal EnableDelayedExpansion
 set "BaseDir=%SystemRoot%\System32\DriverStore\FileRepository"
+set "Target="
 
-for /d %%D in ("%BaseDir%\nv_dispi.inf_amd64_*") do (
+for /d %%D in ("%BaseDir%\nv_disp*.inf_amd64_*") do (
     if exist "%%D\nvlddmkm.sys" (
         set "Target=%%D\nvlddmkm.sys"
-        goto :Apply
     )
 )
 
-exit /b 1
+if not defined Target (
+    echo nvlddmkm.sys not found.
+    timeout /t 3 >nul
+    exit /b 1
+)
 
-:Apply
+echo File found: %Target%
+
 takeown /F "%Target%" /A
-icacls "%Target%" /grant *S-1-5-32-544:F
-icacls "%Target%" /grant *S-1-5-32-545:F
-timeout 3
+icacls "%Target%" /grant *S-1-5-32-544:F >nul
+icacls "%Target%" /grant *S-1-5-32-545:F >nul
+
+echo Permissions successfully applied.
+timeout /t 3 >nul
 exit
