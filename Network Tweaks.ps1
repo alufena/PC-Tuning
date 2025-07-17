9acf1451af582fcf5d1c57771a0712c603c0376b
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-07-17 16:40:22 -0300

    Update Network Tweaks.ps1

--- Network Tweaks.ps1
+++ Network Tweaks.ps1
@@ -1,39 +1,31 @@
-# ------------------------------
-# NETSH & TCP GLOBAL SETTINGS
-# ------------------------------
+netsh interface 6to4 set state disabled
 netsh interface tcp set supplemental Internet congestionprovider=ctcp
+netsh interface teredo set state disabled
+netsh int ip set global loopbackworkercount=16
+netsh int ip set global neighborcachelimit=4096
+netsh int ip set global taskoffload=disabled
+netsh int isatap set state disabled
+netsh int tcp set global autotuninglevel=disabled # disabled/normal
 netsh int tcp set global dca=enabled
-netsh int tcp set global netdma=enabled
-netsh int tcp set global ecncapability=disabled
+netsh int tcp set global ecncapability=disabled # disabled/enabled
 netsh int tcp set global fastopen=enabled
 netsh int tcp set global fastopenfallback=enabled
-netsh int tcp set global rss=enabled
-netsh int tcp set global rsc=disabled
-netsh int tcp set global pacingprofile=always
 netsh int tcp set global hystart=enabled
-netsh int tcp set global autotuninglevel=normal
-netsh int tcp set supplemental internet enablecwndrestart=enabled
+netsh int tcp set global initialRto=3000
+netsh int tcp set global maxsynretransmissions=2
+netsh int tcp set global netdma=enabled
+netsh int tcp set global nonsackrttresiliency=disabled
+netsh int tcp set global pacingprofile=always
+netsh int tcp set global rsc=disabled
+netsh int tcp set global rss=enabled
+netsh int tcp set global timestamps=disabled
+netsh int tcp set security mpp=disabled
+netsh int tcp set security profiles=disabled
 netsh int tcp set supplemental internet congestionprovider=ctcp
-netsh int tcp set security mpp=enabled
-netsh int ip set global neighborcachelimit=4096 taskoffload=disabled
-NETSH interface teredo set state disabled
-NETSH interface 6to4 set state disabled
-NETSH int ip set global loopbackworkercount=16
-NETSH int tcp set global nonsackrttresiliency=disabled
-NETSH int tcp set global timestamps=disabled
-NETSH int tcp set security profiles=disabled
-NETSH int tcp set global initialRto=3000
-NETSH int tcp set global maxsynretransmissions=2
-netsh int isatap set state disabled
+netsh int tcp set supplemental internet enablecwndrestart=enabled
 
-# ------------------------------
-# NETTCPSETTINGS (Modern API)
-# ------------------------------
 Set-NetTCPSetting -SettingName * -AutoTuningLevelLocal Disable -ScalingHeuristics Disabled
 
-# ------------------------------
-# NETWORK ADAPTER SETTINGS
-# ------------------------------
 Disable-NetAdapterPowerManagement *
 Disable-NetAdapterLso -Name *
 Disable-NetAdapterBinding -Name * -ComponentID ms_pacer
@@ -41,24 +33,20 @@ Disable-NetAdapterBinding -Name * -ComponentID ms_pacer
 # If you share files on the network, DO NOT disable the client below
 Disable-NetAdapterBinding -Name * -ComponentID ms_msclient
 
-# Offload Settings — some enabled for network performance, others disabled for stability
 Set-NetOffloadGlobalSetting -PacketCoalescingFilter disabled -Chimney Disabled -Taskoffload Enabled -ReceiveSideScaling Enabled -ReceiveSegmentCoalescing Disabled
 
 Set-NetAdapterRdma -Name "*" -Enabled $True
 Set-NetAdapterRss -Name "*" -Profile Conservative
 Set-NetAdapterIPsecOffload -Name "*" -Enabled $True
 Enable-NetAdapterChecksumOffload -Name "*"
+Enable-NetAdapterEncapsulatedPacketTaskOffload -Name "*"
 Enable-NetAdapterIPsecOffload -Name "*"
+Enable-NetAdapterQos -Name "*"
 Enable-NetAdapterRsc -Name "*"
 Enable-NetAdapterRss -Name "*"
-Enable-NetAdapterQos -Name "*"
-Enable-NetAdapterEncapsulatedPacketTaskOffload -Name "*"
 Enable-NetAdapterSriov -Name "*"
 Enable-NetAdapterVmq -Name "*"
 
-# ------------------------------
-# TCP/IP REGISTRY SETTINGS
-# ------------------------------
 function Set-RegKey {
     param (
         [string]$path,
@@ -73,7 +61,6 @@ function Set-RegKey {
     }
 }
 
-# Disable Nagle and ACK delay
 $tcpipPath = "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces"
 $tcpInterfaces = Get-ChildItem -Path $tcpipPath
 foreach ($interface in $tcpInterfaces) {
@@ -82,33 +69,27 @@ foreach ($interface in $tcpInterfaces) {
     Set-RegKey -path $interface.PSPath -name "TcpDelAckTicks" -value 0
 }
 
-# NetBIOS: Disable for reduced local overhead
 $netbtPath = "HKLM:\SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
 $interfaces = Get-ChildItem -Path $netbtPath
 foreach ($interface in $interfaces) {
     Set-RegKey -path $interface.PSPath -name "NetbiosOptions" -value 2
 }
 
-# ------------------------------
-# DRIVER-SPECIFIC NIC TWEAKS
-# ------------------------------
+
 $driverPath = "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces"
 $networkInterfaces = Get-ChildItem -Path $driverPath
 foreach ($interface in $networkInterfaces) {
-    # Flow Control
+
     Set-RegKey -path $interface.PSPath -name "*FlowControl" -value "0" -type "String"
     Set-RegKey -path $interface.PSPath -name "FlowControlCap" -value "0" -type "String"
 
-    # Interrupt Delay (Intel NICs)
     Set-RegKey -path $interface.PSPath -name "TxIntDelay" -value "0" -type "String"
     Set-RegKey -path $interface.PSPath -name "TxAbsIntDelay" -value "0" -type "String"
     Set-RegKey -path $interface.PSPath -name "RxIntDelay" -value "0" -type "String"
     Set-RegKey -path $interface.PSPath -name "RxAbsIntDelay" -value "0" -type "String"
 
-    # Interrupt Moderation
     Set-RegKey -path $interface.PSPath -name "*InterruptModeration" -value "0" -type "String"
 
-    # Green Ethernet and power-saving
     Set-RegKey -path $interface.PSPath -name "*EEE" -value "0" -type "String"
     Set-RegKey -path $interface.PSPath -name "AdvancedEEE" -value "0" -type "String"
     Set-RegKey -path $interface.PSPath -name "AutoPowerSaveModeEnabled" -value "0" -type "String"
@@ -118,9 +99,6 @@ foreach ($interface in $networkInterfaces) {
     Set-RegKey -path $interface.PSPath -name "ULPMode" -value "0" -type "String"
 }
 
-# ------------------------------
-# OPTIONAL: ENABLE DCTCP PROFILES
-# ------------------------------
 # netsh int tcp set supplemental internet congestionprovider=dctcp
 
 Disable-NetAdapterBinding -Name * -DisplayName 'Client for Microsoft Networks'
