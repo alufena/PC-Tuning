365ee585134da8be8ceb556026708f13a48d3aad
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-08-16 23:00:17 -0300

    Update Network Tweaks.ps1

--- Network Tweaks.ps1
+++ Network Tweaks.ps1
@@ -5,27 +5,33 @@ netsh int ip set global taskoffload=disabled
 netsh int ip set interface ethernet currenthoplimit=64
 netsh int isatap set state disabled
 netsh int tcp set global autotuninglevel=normal # disabled/normal
+netsh int tcp set global chimney=disabled
+netsh int tcp set global congestionprovider=cubic # default/cubic
 netsh int tcp set global dca=disabled # disabled/enabled
 netsh int tcp set global ecncapability=disabled # disabled/enabled
 netsh int tcp set global fastopen=enabled
 netsh int tcp set global fastopenfallback=enabled
 netsh int tcp set global hystart=enabled
-netsh int tcp set global initialRto=2000 # 2000/3000
-netsh int tcp set global maxsynretransmissions=2
+netsh int tcp set global initialRto=3000 # 2000/3000
+netsh int tcp set global maxsynretransmissions=3 # 2/3/4
 netsh int tcp set global netdma=enabled
 netsh int tcp set global nonsackrttresiliency=disabled
-netsh int tcp set global pacingprofile=always
+netsh int tcp set global pacingprofile=always # always/off
+netsh int tcp set global prr=enabled
 netsh int tcp set global rsc=disabled # disabled/enabled
 netsh int tcp set global rss=enabled
-netsh int tcp set global timestamps=disabled
+netsh int tcp set global timestamps=disabled # disabled/enabled
 netsh int tcp set security mpp=disabled
 netsh int tcp set security profiles=disabled
-netsh int tcp set supplemental internet congestionprovider=ctcp # ctcp/dctcp
-# netsh int tcp set supplemental internet congestionprovider=dctcp
+netsh int tcp set supplemental internet congestionprovider=cubic # ctcp/dctcp/newreno/cubic
 netsh int tcp set supplemental internet enablecwndrestart=enabled
+netsh int tcp set supplemental template=custom icw=10
 netsh int teredo set state disabled
+netsh int udp set global uro=disabled
+netsh advfirewall firewall set rule group="Network Discovery" new enable=No
 
-# w11 only
+Disable-NetAdapterBinding -Name "*" -ComponentId 'vmware_bridge','ms_lldp','ms_lltdio','ms_implat','ms_rspndr','ms_server','ms_msclient'
+
 # netsh int tcp set supplemental Template=Automatic CongestionProvider=cubic
 # netsh int tcp set supplemental Template=Compat CongestionProvider=bbr2
 # netsh int tcp set supplemental Template=Compat CongestionProvider=cubic
@@ -38,9 +44,16 @@ netsh int teredo set state disabled
 # netsh int tcp set supplemental Template=InternetCustom CongestionProvider=bbr2
 # netsh int tcp set supplemental Template=InternetCustom CongestionProvider=cubic
 
-Set-NetTCPSetting -SettingName "*" -AutoTuningLevelLocal Normal -ScalingHeuristics Disabled -MinRto 300
+# w11 only
+# netsh int tcp set supplemental Template=Internet CongestionProvider=BBR
+# netsh int tcp set supplemental Template=Datacenter CongestionProvider=BBR
+# netsh int tcp set supplemental Template=Compat CongestionProvider=BBR
+# netsh int tcp set supplemental Template=DatacenterCustom CongestionProvider=BBR
+# netsh int tcp set supplemental Template=InternetCustom CongestionProvider=BBR
+# netsh int tcp set supplemental Template=Automatic CongestionProvider=BBR
 
-Set-NetTCPSetting -SettingName "*" -MinRto 300
+Set-NetTCPSetting -SettingName "*" -AutoTuningLevelLocal Normal -ScalingHeuristics Disabled
+Set-NetTCPSetting -SettingName internet -MinRto 300
 
 Disable-NetAdapterPowerManagement -Name "*"
 Disable-NetAdapterLso -Name "*"
@@ -49,7 +62,7 @@ Disable-NetAdapterBinding -Name "*" -ComponentID ms_pacer
 # If you share files on the network, DO NOT disable the client below
 Disable-NetAdapterBinding -Name "*" -ComponentID ms_msclient
 
-Set-NetOffloadGlobalSetting -PacketCoalescingFilter disabled -Chimney Disabled -Taskoffload Disabled -ReceiveSideScaling Enabled -ReceiveSegmentCoalescing Disabled # Taskoffload Disabled/Enabled
+Set-NetOffloadGlobalSetting -PacketCoalescingFilter Disabled -Chimney Disabled -Taskoffload Disabled -ReceiveSideScaling Enabled -ReceiveSegmentCoalescing Disabled # Taskoffload Disabled/Enabled, ReceiveSideScaling Enabled/Disabled
 
 Set-NetAdapterRdma -Name "*" -Enabled $True
 Set-NetAdapterRss -Name "*" -Profile Conservative
