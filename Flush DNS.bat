ed64c90baab3534bc402f0e4fee1e28911d1f762
Anderson Pedro <55760228+alufena@users.noreply.github.com>
2025-09-25 23:39:12 -0300

    Update Flush DNS.bat

--- Flush DNS.bat
+++ Flush DNS.bat
@@ -1,9 +1,13 @@
 ipconfig /flushdns
 ipconfig /registerdns
 nbtstat -rr
-netsh winsock reset catalog
-netsh int ipv4 reset reset.log
-netsh int ipv6 reset reset.log
-netsh int ip reset all
+::netsh int ip reset
+netsh int ipv4 reset
+netsh int ipv6 reset
+::netsh int tcp reset
 netsh winsock reset
+::netsh advfirewall reset
+netsh branchcache reset
+netsh http flush logbuffer
+netsh winhttp reset proxy
 exit
